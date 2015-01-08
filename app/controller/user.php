ce2afffedb964f0676e9015dca1a1d577f161f7b
作芒 <zuomang.xzm@alibaba-inc.com>
2015-01-08 14:54:05 +0800

    clear with remote origin

--- app/controller/user.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class user {
-    public function index() {
-        echo 'this is index ...';
-    }
-}
--- index.php
+++ index.php
@@ -1,25 +1,82 @@
 <?php
 
-$xhprof = true;
-
-
-$f3 = require 'lib/fatfree/base.php';
+// Kickstart the framework
+$f3=require('lib/base.php');
 
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
 
+// Load configuration
 $f3->config('config.ini');
-$f3->set('AUTOLOAD','app/controller/;app/controller/admin/;app/view/');
-$f3->route('GET|POST /@controller/@action', '@controller->@action');
-$f3->route('GET|POST /admin/', "admin\panel->dashboard");
-$f3->route('GET|POST /admin/login*', "admin\user->login");
-$f3->route('GET|POST /admin/@controller/@action', 'admin\@controller->@action');
 
-$db = new DB\SQL(
-    'mysql:host=localhost;port=3306;dbname=taskiller',
-    'root',
-    'root'
+$f3->route('GET /',
+	function($f3) {
+		$classes=array(
+			'Base'=>
+				array(
+					'hash',
+					'json',
+					'session'
+				),
+			'Cache'=>
+				array(
+					'apc',
+					'memcache',
+					'wincache',
+					'xcache'
+				),
+			'DB\SQL'=>
+				array(
+					'pdo',
+					'pdo_dblib',
+					'pdo_mssql',
+					'pdo_mysql',
+					'pdo_odbc',
+					'pdo_pgsql',
+					'pdo_sqlite',
+					'pdo_sqlsrv'
+				),
+			'DB\Jig'=>
+				array('json'),
+			'DB\Mongo'=>
+				array(
+					'json',
+					'mongo'
+				),
+			'Auth'=>
+				array('ldap','pdo'),
+			'Bcrypt'=>
+				array(
+					'mcrypt',
+					'openssl'
+				),
+			'Image'=>
+				array('gd'),
+			'Lexicon'=>
+				array('iconv'),
+			'SMTP'=>
+				array('openssl'),
+			'Web'=>
+				array('curl','openssl','simplexml'),
+			'Web\Geo'=>
+				array('geoip','json'),
+			'Web\OpenID'=>
+				array('json','simplexml'),
+			'Web\Pingback'=>
+				array('dom','xmlrpc')
+		);
+		$f3->set('classes',$classes);
+		$f3->set('content','welcome.htm');
+		echo View::instance()->render('layout.htm');
+	}
+);
+
+$f3->route('GET /userref',
+	function($f3) {
+		$f3->set('content','userref.htm');
+		echo View::instance()->render('layout.htm');
+	}
 );
 
 $f3->run();
